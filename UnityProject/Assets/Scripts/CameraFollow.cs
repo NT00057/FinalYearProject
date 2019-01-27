79a90c58ed12ded26d1acc05d44e78313ca284ca
Nick Tauwhare <nt00057@surrey.ac.uk>
2019-01-27 19:32:29 +0000

    Tracking camera set-up

---
+++ UnityProject/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Controller2D target;
+    public float verticalOffset;
+    public float lookAheadDstX;
+    public float lookSmoothTimeX;
+    public float verticalSmoothTime;
+    public Vector2 focusAreaSize;
+
+
+
+    float currentLookAheadX;
+    float targetLookAheadX;
+    float lookAheadDirX;
+    float smoothLookVelocityX;
+    float smoothVelocityY;
+
+    bool lookAheadStopped;
+
+
+
+    void LateUpdate()
+    {
+        focusArea.Update(target.collider.bounds);
+
+        Vector2 focusPosition = focusArea.centre + Vector2.up * verticalOffset;
+
+        if (focusArea.velocity.x != 0)
+        {
+            lookAheadDirX = Mathf.Sign(focusArea.velocity.x);
+            if (Mathf.Sign(target.playerInput.x) == Mathf.Sign(focusArea.velocity.x) && target.playerInput.x != 0)
+            {
+                lookAheadStopped = false;
+                targetLookAheadX = lookAheadDirX * lookAheadDstX;
+            }
+            else
+            {
+                if (!lookAheadStopped)
+                {
+                    lookAheadStopped = true;
+                    targetLookAheadX = currentLookAheadX + (lookAheadDirX * lookAheadDstX - currentLookAheadX) / 4f;
+                }
+            }
+        }
+
+
+        currentLookAheadX = Mathf.SmoothDamp(currentLookAheadX, targetLookAheadX, ref smoothLookVelocityX, lookSmoothTimeX);
+
+        focusPosition.y = Mathf.SmoothDamp(transform.position.y, focusPosition.y, ref smoothVelocityY, verticalSmoothTime);
+        focusPosition += Vector2.right * currentLookAheadX;
+        transform.position = (Vector3)focusPosition + Vector3.forward * -10;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, .5f);
+        Gizmos.DrawCube(focusArea.centre, focusAreaSize);
+    }
+}
